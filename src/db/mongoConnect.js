8be98f1dece89f8b9231f01092a36c051aa9d388
Trishank Sharma <trishank7409@gmail.com>
2024-02-19 19:16:02 +0530

    Update MongoDB dependency and connect to the database. (generated by blackbox.ai )

--- src/db/mongoConnect.js
+++ src/db/mongoConnect.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 import 'dotenv/config'
-
+console.log(process.env.MONGODB_URI)
 const connectDB= async()=>{
     try {
-      const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}`)
+
       console.log(`\n MongoDb connected !! DB HOST : ${connectionInstance.connection.host}`)
     
 
